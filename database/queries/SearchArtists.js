b59aa96763139ea12642b5da54202d42bd826a1f
Wesley <wesleyrasada@gmail.com>
2017-03-14 20:45:59 -0700

    MongoDB - Search Artist Criteria query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -33,13 +33,18 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 const buildQuery = (criteria) => {
   const query = {};
 
-  if(criteria.age) {
+  if (criteria.name) {
+    query.$text = { $search: criteria.name }
+  }
+
+  if (criteria.age) {
     query.age = {
       $gte: criteria.age.min,
       $lte: criteria.age.max
       }
     }
-  if(criteria.yearsActive) {
+
+  if (criteria.yearsActive) {
     query.yearsActive = {
       $gte: criteria.yearsActive.min,
       $lte: criteria.yearsActive.max
